a383cc7fbe2e1adb9d217e77b411a2abdb008f63
Jiwon Jeon <jwurbane97@ajou.ac.kr>
2019-11-09 17:49:42 +0900

    add: 404, 500

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -16,15 +16,51 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 # DB Models
+class College(db.Model):
+    __tablename__ = 'colleges'
+    college_code = db.Column(db.String(5), primary_key=True)
+    college_nm = db.Column(db.String(256))
+
+    def __repr__(self):
+        return '<College %r>' % self.college_code
+
 class User(db.Model):
     __tablename__ = 'users'
-    uuid = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(256), index=True, unique=True)
+    user_idx = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(256), index=True, unique=True)
+    user_id = db.Column(db.String(256), index=True, unique=True)
+    password = db.Column(db.String(256))
+    user_nm = db.Column(db.String(256))
+    identity_num = db.Column(db.Integer, index=True, unique=True)
+    user_type = db.Column(db.String(256))
+    user_status = db.Column(db.String(1))
+    policy_yn = db.Column(db.Boolean)
+    college_code = db.Column(db.String(5))
+    college_nm = db.Column(db.String(256))
+    dpt_code = db.Column(db.String(256))
+    dpt_nm = db.Column(db.String(256))
+    auth_email_yn = db.Column(db.Boolean)
+    auth_token = db.Column(db.String(256))
+    auth_status = db.Column(db.String(1))
+    user_profile = db.Column(db.String(256))
+    nick_nm = db.Column(db.String(50))
+    bamboo_spear = db.Column(db.Integer)
+    reg_ip = db.Column(db.String(15))
+    reg_dt = db.Column(db.String(256))
+    upt_ip = db.Column(db.String(15))
+    upt_dt = db.Column(db.String(256))
+    log_ip = db.Column(db.String(15))
+    log_dt = db.Column(db.String(256))
     
     def __repr__(self):
         return '<User %r>' % self.username
 
 # Schema Objects
+class CollegeObject(SQLAlchemyObjectType):
+    class Meta:
+        model = College
+        interfaces = (graphene.relay.Node, )
+
 class UserObject(SQLAlchemyObjectType):
     class Meta:
         model = User
@@ -33,5 +69,6 @@ class UserObject(SQLAlchemyObjectType):
 class Query(graphene.ObjectType):
     node = graphene.relay.Node.Field()
     all_users = SQLAlchemyConnectionField(UserObject)
+    all_colleges = SQLAlchemyConnectionField(CollegeObject)
 
 schema = graphene.Schema(query=Query)
--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import About from './views/About.vue'
+import NotFound from './views/NotFound.vue'
+import ServError from './views/ServError.vue'
 
 Vue.use(Router)
 
@@ -10,6 +13,25 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    },
+    {
+      path: '/error',
+      name: 'serv_error',
+      component: ServError
+    },
+    {
+      path: '/404',
+      name: 'not_found',
+      component: NotFound
+    },
+    {
+      path: '*',
+      redirect: '/404'
     }
   ]
 })
