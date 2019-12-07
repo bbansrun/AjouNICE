81727097bba28545879b4c9f34c23ec6d4f4939e
Tommy J <jwurbane97@ajou.ac.kr>
2019-12-08 00:13:53 +0900

    fix: npm script aspect of server

--- server/app/config.py
+++ server/app/config.py
@@ -2,6 +2,9 @@ import os
 from app import app
 
 class Config(object):
+    FLASK_ENV =  os.getenv('FLASK_ENV', 'production')
+    SECRET_KEY = os.getenv('FLASK_SECRET', 'Secret')
+    
     APP_DIR = os.path.dirname(__file__)
     ROOT_DIR = os.path.dirname(APP_DIR)
     DIST_DIR = os.path.join(ROOT_DIR, 'dist')
