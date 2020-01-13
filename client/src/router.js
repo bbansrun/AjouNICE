31cf4df3bf47a17aa3d09c350e8a4053a34d83bb
G1 <g1.jeon@kakao.com>
2020-01-13 18:27:58 +0900

    fix: Client View Hierarchy 조정

--- client/src/router.js
+++ client/src/router.js
@@ -1,17 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
-import ErrorPage from './views/ErrorPage.vue'
-import AdminLogin from './views/AdminLogin.vue'
-import Login from './views/Login.vue'
-import RenewAccount from './views/RenewAccount.vue'
-import Signup from './views/SignUp.vue'
-import Unauthorized from './views/Unauthorized.vue'
-import Authorize from './views/Authorize.vue'
-import Board from './views/Board.vue'
-import Policy from './views/Policy.vue'
-import Contact from './views/Contact.vue'
+import Home from './views/base/Home.vue'
+import About from './views/base/About.vue'
+import ErrorPage from './views/base/ErrorPage.vue'
+import Policy from './views/base/Policy.vue'
+import Contact from './views/base/Contact.vue'
+import AdminLogin from './views/auth/AdminLogin.vue'
+import Login from './views/auth/Login.vue'
+import RenewAccount from './views/auth/RenewAccount.vue'
+import Signup from './views/auth/SignUp.vue'
+import Unauthorized from './views/auth/Unauthorized.vue'
+import Authorize from './views/auth/Authorize.vue'
+import Board from './views/board/Board.vue'
+import PostView from './views/board/View.vue'
+import Edit from './views/board/Edit.vue'
+import Dashboard from './views/admin/Dashboard.vue'
+import Profile from './views/user/Profile.vue'
+import ProfileEdit from './views/user/Edit.vue'
 
 Vue.use(Router)
 
@@ -63,6 +68,31 @@ export default new Router({
       beforeEnter: requireAuth
     },
     {
+      path: '/board/:category/:name/:post_id/view',
+      component: PostView,
+      beforeEnter: requireAuth
+    },
+    {
+      path: '/board/:category/:name/new',
+      component: Edit,
+      beforeEnter: requireAuth
+    },
+    {
+      path: '/board/:category/:name/:post_id/edit',
+      component: Edit,
+      beforeEnter: requireAuth
+    },
+    {
+      path: '/profile/:user_id/',
+      component: Profile,
+      beforeEnter: requireAuth
+    },
+    {
+      path: '/profile/:user_id/edit',
+      component: ProfileEdit,
+      beforeEnter: requireAuth
+    },
+    {
       path: '/auth/authorize',
       name: 'authorize',
       component: Authorize
@@ -80,6 +110,7 @@ export default new Router({
     {
       path: '/gate/manager',
       name: 'admin',
+      component: Dashboard,
       beforeEnter: requireAdminAuth
     },
     {
---
+++ client/src/views/admin/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="wrapper">
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Footer from '@/components/Footer.vue'
+export default {
+    components: {
+        Footer
+    }
+}
+</script>
