cace6b9580da65cf6652b45091eb71352b72d7a3
G1 <g1.jeon@kakao.com>
2020-01-14 19:43:42 +0900

    fix: Styles

--- client/src/components/Error.vue
+++ client/src/components/Error.vue
@@ -1,20 +1,20 @@
 <template>
-    <section data-error>
-        <header>
-            <h1 class="logo-font">
+    <section id="error">
+        <header id="error_code">
+            <h1 data-logo>
                 <span>X_X</span> | <span class="code" v-if="code">{{ code }}</span>
             </h1>
         </header>
-        <div align="center" data-description>
-            <p>{{ description }}</p>
+        <div align="center" id="error_desc">
+            <header>{{ description }}</header>
             <p>{{ sub_description }}</p>
+            <a v-if="code == 500" href="mailto:team.ajounice@gmail.com">오류 리포트</a>
         </div>
-        <a href="/" class="button">
-            → 처음으로
-        </a>
-        <hr />
+        <nav id="error_nav">
+            <router-link to="/" class="btn rounded-on-hover box-shadow-on-hover text-inverse">→ 처음으로</router-link>
+        </nav>
         <div class="logo">
-            <h2 class="logo-font">AjouNICE!</h2>
+            <h2 data-logo>AjouNICE!</h2>
             <span text-divider>|</span>
             <span>아주대학교 커뮤니티 서비스</span>
         </div>
@@ -33,54 +33,14 @@ export default {
 }
 </script>
 
-<style lang="scss">
-@import "~@/assets/styles/reset";
-@import "~@/assets/styles/index";
-@import "~@/assets/styles/fonts";
-
-section[data-error] {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    width: 100vw;
-    height: 100vh;
-    justify-content: center;
-    align-items: center;
-    background: #fff;
-    > header > h1 {
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        background-clip: text;
-        font-size: 4rem;
-        font-weight: bold;
-        background-image: linear-gradient(to bottom, #a18cd1 0%, #fbc2eb 100%);
-        text-shadow: 0 2px 2px rgba(0,0,0,.1);
-    }
-    > .button {
-        font-size: .8rem;
-        margin-top: 15px;
+<style lang="scss" scoped>
+.btn {
+    padding: .6rem 3rem;
+    margin-top: 1rem;
+    background: #a18cd1;
+    &:hover {
         color: #fff;
-        background: #a18cd1;
-        border-radius: 8px;
-        padding: 8px 30px;
-        text-decoration: none;
-        box-shadow: 0 5px 5px rgba(0,0,0,.2);
-        transition: .2s all ease;
-        &:hover {
-            background: purple;
-            transform: translateY(3px);
-        }
-    }
-    > .logo {
-        text-align: center;
-        > h2 {
-            display: inline-block;
-            font-size: 1.5rem;
-            color: rgb(41, 61, 81);
-        }
-        > span {
-            font-size: .8rem;
-        }
+        background: purple;
     }
 }
 </style>
