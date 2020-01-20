566ce04772e7dd9f9991f2f0461ac0c5563e6f4d
nailerHeum <kingman330@gmail.com>
2020-01-20 16:05:59 +0900

    FEAT client side encrypted requests

--- client/src/views/user/Profile.vue
+++ client/src/views/user/Profile.vue
@@ -13,27 +13,27 @@ import Navigation from '@/components/Navigation.vue'
 import Landing from '@/components/Landing.vue'
 import Footer from '@/components/Footer.vue'
 export default {
-    components: {
-        Navigation,
-        Landing,
-        Footer
-    },
-    data() {
-        return {
-            scrollBase: null,
-            user_nm: null
-        }
-    },
-    beforeMount() {
-        let _this = this
-        this.$apollo.query({
-            query: gql `{ findUserByIdx(user_idx: ${_this.$route.params.user_id}) { user_nm } }`
-        }).then(result => {
-            _this.user_nm = result.data.findUserByIdx.user_nm
-        })
-    },
-    mounted () {
-        this.scrollBase = this.$refs.scrollBase.$el.getBoundingClientRect().bottom / 3
+  components: {
+    Navigation,
+    Landing,
+    Footer
+  },
+  data () {
+    return {
+      scrollBase: null,
+      user_nm: null
     }
+  },
+  beforeMount () {
+    let _this = this
+    this.$apollo.query({
+      query: gql`{ findUserByIdx(user_idx: ${_this.$route.params.user_id}) { user_nm } }`
+    }).then(result => {
+      _this.user_nm = result.data.findUserByIdx.user_nm
+    })
+  },
+  mounted () {
+    this.scrollBase = this.$refs.scrollBase.$el.getBoundingClientRect().bottom / 3
+  }
 }
 </script>
