f47160f5eca0fc4cdbbfd43a90fe0468faee55af
nailerHeum <kingman330@gmail.com>
2020-01-24 12:18:18 +0900

    FEAT context pattern in apollo server

--- apollo-server/apollo.js
+++ apollo-server/apollo.js
@@ -2,17 +2,26 @@
 const { ApolloServer, gql } = require('apollo-server')
 const { RedisCache } = require('apollo-server-cache-redis')
 const server = new ApolloServer({
-    typeDefs: gql(require('./typeDefs')),
-    resolvers: require('./resolvers'),
-    persistedQueries: {
-        cache: new RedisCache({
-            host: require('./config/config.json')['development']['redis_host']
-        })
-    },
-    cors: true,
-    context: ({ req, res}) => {
-
-    },
+  typeDefs: gql(require('./typeDefs')),
+  resolvers: require('./resolvers'),
+  persistedQueries: {
+    cache: new RedisCache({
+        host: require('./config/config.json')['development']['redis_host']
+    })
+  },
+  cors: true,
+  context: ({ req }) => {
+    try {
+      if (req.get("content-transfer-encoding") === "base64") {
+        const str = await decodeTextBody(req.body); // decoding
+        req.body = JSON.parse(str);
+        req.headers["content-type"] = "application/json";
+        req.graphqlWasEncoded = true;
+      }
+    } catch (e) {
+      throw new Error(e);
+    }
+  },
 })
 
 server.listen({ port: 455 }).then(({ url }) => {
