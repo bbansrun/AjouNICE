7a8ce5198ddb4b25d6f99b3ee2ee85a1a20765c9
nailerHeum <kingman330@gmail.com>
2020-01-29 15:56:33 +0900

    ADD rules for eslintrc

--- apollo-server/apollo.js
+++ apollo-server/apollo.js
@@ -1,31 +1,31 @@
 
-const { ApolloServer, gql } = require('apollo-server-express')
-const { RedisCache } = require('apollo-server-cache-redis')
-const { LoggerExtension } = require('apollo-server-logger')
-const app = require('./middlewares/app')
+const { ApolloServer, gql, } = require('apollo-server-express');
+const { RedisCache, } = require('apollo-server-cache-redis');
+const { LoggerExtension, } = require('apollo-server-logger');
+const app = require('./middlewares/app');
 
 const server = new ApolloServer({
   typeDefs: gql(require('./typeDefs')),
   resolvers: require('./resolvers'),
   persistedQueries: {
     cache: new RedisCache({
-      host: require('./config/config.json').development.redis_host
-    })
+      host: require('./config/config.json').development.redis_host,
+    }),
   },
   cors: true,
   cacheControl: {
-    defaultMaxAge: 10
+    defaultMaxAge: 10,
   },
   extensions: [() => new LoggerExtension({
-    tracing: true
+    tracing: true,
   })],
-  formatResponse: (res) => {
-    res.hithisisnailer = 'hihi'
-    return res
-  }
-})
-server.applyMiddleware({ app, cors: false })
-app.listen(455)
+  formatResponse: res => {
+    res.hithisisnailer = 'hihi';
+    return res;
+  },
+});
+server.applyMiddleware({ app, cors: false, });
+app.listen(455);
 
 //   (app) => {
 //     console.log('========================== AjouNICE! ==========================')
