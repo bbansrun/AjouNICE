8d40755a5cf30ea2555717d547225154203dfa60
nailerHeum <kingman330@gmail.com>
2020-01-29 16:34:11 +0900

    FEAT server side encryption
      임시 key 제외시키고 config module 적용 필요함

--- apollo-server/apollo.js
+++ apollo-server/apollo.js
@@ -2,6 +2,8 @@
 const { ApolloServer, gql, } = require('apollo-server-express');
 const { RedisCache, } = require('apollo-server-cache-redis');
 const { LoggerExtension, } = require('apollo-server-logger');
+
+const { encodeTextBody, } = require('./middlewares/securityModule');
 const app = require('./middlewares/app');
 
 const server = new ApolloServer({
@@ -19,9 +21,25 @@ const server = new ApolloServer({
   extensions: [() => new LoggerExtension({
     tracing: true,
   })],
-  formatResponse: res => {
-    res.hithisisnailer = 'hihi';
-    return res;
+  formatResponse: async res => {
+    try {
+      let type = 'application/json';
+      if (res.graphqlWasEncoded) {
+        res.data = await encodeTextBody(res.data); // encoding
+        type = 'text/plain';
+        res.setHeader('content-transfer-encoding', 'base64');
+      }
+      res.setHeader('content-type', type);
+      res.setHeader(
+        'content-length',
+        Buffer.byteLength(res, 'utf8').toString()
+      );
+      return res;
+    } catch (error) {
+      res.setHeader('Content-Type', 'application/json');
+      res.setHeader('Content-Length', Buffer.byteLength(res, 'utf8').toString());
+      return res;
+    }
   },
 });
 server.applyMiddleware({ app, cors: false, });
