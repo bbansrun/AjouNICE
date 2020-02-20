9666763cf70887d09b431d42a62e2d4c17feb41a
G1 <g1.jeon@kakao.com>
2020-02-20 16:53:26 +0900

    fix: 게시물 작성시 upt_ip 반영

--- apollo-server/typeDefs.js
+++ apollo-server/typeDefs.js
@@ -183,7 +183,7 @@ type Mutation {
     lastLogin(userId: String!, ip: String!): Boolean
     authorize(user_idx: Int!): Boolean
     resetEmailToken(email: String!): Boolean
-    writePost(category_idx: Int!, user_idx: Int!, nick_nm: String, title: String, body: String, reg_ip: String): Board
+    writePost(category_idx: Int!, user_idx: Int!, nick_nm: String, title: String, body: String, reg_ip: String!, upt_ip: String!): Board
     removePost(board_idx: Int!): Boolean
     editPost(board_idx: Int!, category_idx: Int!, user_idx: Int!, nick_nm: String, title: String, body: String, upt_ip: String, upt_dt: Date): Board
     writeReply(board_idx: Int!, user_idx: Int!, text: String, reg_ip: String!, upt_ip: String!): BoardComment
