eeb398be86394c496f844ecebaa9e6ed07d4bde8
nailerHeum <kingman330@gmail.com>
2020-02-24 21:17:06 +0900

    FEAT sorting notices by notice number
      특이사항:
            1. 의과대학 쪽, 소프트웨어학과는 다른 id를 기준으로 정렬됨
            2. type1은 article_no, type2는 ai(article_id), type3는 num
               이지만 모두 article_no 라는 key로 값을 전달함.

--- server/app/api/customutils/crawl_utils.py
+++ server/app/api/customutils/crawl_utils.py
@@ -1,12 +1,24 @@
 import requests
 import json
 import os
+from urllib.parse import urlparse, parse_qs
 from bs4 import BeautifulSoup
 
 CURRENT_DIR = os.path.realpath(os.path.dirname(__file__))
 CONSTANTS_DIR = os.path.join(CURRENT_DIR, "./constants/")
 url_list = json.load(open(os.path.join(CONSTANTS_DIR, "url_list.json")))
 
+# type 1
+def get_article_no(url_string):
+    return parse_qs(urlparse(url_string).query)['article_no'][0]
+
+# type 2
+def get_article_id(url_string):
+    return parse_qs(urlparse(url_string).query)['ai'][0]
+
+# type 3
+def get_article_num(url_string):
+    return parse_qs(urlparse(url_string).query)['num'][0]
 
 def parse_content(code):
     if (code in list(map(lambda x: x['code'], url_list['type1']))):
@@ -27,25 +39,25 @@ def parse_content(code):
             BeautifulSoup(res.text, 'html.parser'), target)
     return None
 
-
 def parse_content_type1(soup, urlInfo):
     posts = []
     for post in soup.select('#jwxe_main_content > div > div.list_wrap > table > tbody > tr'):
         try:
             title = post.find('td', class_='title_comm').a.text.strip()
-            link = urlInfo['link'] + \
-                post.find('td', class_='title_comm').a['href']
+            link_path = post.find('td', class_='title_comm').a['href']
+            link = urlInfo['link'] + link_path
+            article_no = get_article_no(link_path)
             posts.append({
                 'unit': urlInfo['name'],
                 'code': urlInfo['code'],
                 'boardName': urlInfo['boardName'],
                 'title': title,
-                'link': link
+                'link': link,
+                'article_no': article_no,
             })
         except:
             pass
     return posts
-
 
 def parse_content_type2(soup, urlInfo):
     posts = []
@@ -54,17 +66,21 @@ def parse_content_type2(soup, urlInfo):
             title = post.find('td', class_='aleft').a.text.strip()
             link = '/'.join(urlInfo['link'].split('/')[:-1]) + \
                 '/' + post.find('td', class_='aleft').a['href']
+            try:
+                article_id = get_article_id(link)
+            except:
+                print(link)
             posts.append({
                 'unit': urlInfo['name'],
                 'code': urlInfo['code'],
                 'boardName': urlInfo['boardName'],
                 'title': title,
-                'link': link
+                'link': link,
+                'article_no': article_id
             })
         except:
             pass
     return posts
-
 
 def parse_content_type3(soup, urlInfo):
     posts = []
@@ -74,15 +90,18 @@ def parse_content_type3(soup, urlInfo):
             title = post.find('td', attrs={"align": "left"}).a.text.strip()
             link = 'http://software.ajou.ac.kr' + \
                 post.find('td', attrs={"align": "left"}).a['href']
+            article_num = get_article_num(link)    
             posts.append({
                 'unit': urlInfo['name'],
                 'code': urlInfo['code'],
                 'boardName': urlInfo['boardName'],
                 'title': title,
-                'link': link
+                'link': link,
+                'article_no': article_num
             })
         except:
             pass
+    posts.sort(key=lambda x: x['article_no'], reverse=True)
     return posts
 
 def get_schedule():
@@ -100,6 +119,6 @@ def get_schedule():
                 'etc': etc,
             })
         except:
-            print(row, 'EXCEPTION@@@@@@@@@@@@@@@@@')
+            pass
     return results
             
