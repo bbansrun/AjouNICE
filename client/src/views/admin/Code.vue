7dc5bc8d3bf12cd46c49145214e6f08bfc3f0779
G1 <g1.jeon@kakao.com>
2020-02-24 23:30:18 +0900

    fix: 관리자 일부 컴포넌트 테이블 조정

--- client/src/views/admin/Code.vue
+++ client/src/views/admin/Code.vue
@@ -1,11 +1,62 @@
 <template>
   <section class="codes">
-      <header class="underline underline-inline-block underline-animated">
-          <strong>학부/학과 코드관리</strong>
-      </header>
-      <article>
-          <b-table :columns="columns" :data="data" :mobile-cards="false" />
-      </article>
+    <header class="underline underline-inline-block underline-animated">
+      <strong>학부/학과 코드관리</strong>
+    </header>
+    <article>
+      <b-table
+        :data="data"
+        :mobile-cards="false"
+      >
+        <template slot-scope="props">
+          <b-table-column
+            field="type"
+            label="구분"
+            sortable
+          >
+            {{ props.row.type }}
+          </b-table-column>
+
+          <b-table-column
+            field="code"
+            label="코드"
+            sortable
+          >
+            {{ props.row.code }}
+          </b-table-column>
+
+          <b-table-column
+            field="value"
+            label="값"
+            sortable
+          >
+            <strong>{{ props.row.value }}</strong>
+          </b-table-column>
+
+          <b-table-column
+            field="settings"
+            label="설정"
+          >
+            <div class="buttons">
+              <b-button
+                type="is-warning"
+                size="is-small"
+              >
+                <font-awesome-icon icon="exclamation-triangle" />&nbsp;
+                <span>수정</span>
+              </b-button>
+              <b-button
+                type="is-danger"
+                size="is-small"
+              >
+                <font-awesome-icon icon="times" />&nbsp;
+                <span>삭제</span>
+              </b-button>
+            </div>
+          </b-table-column>
+        </template>
+      </b-table>
+    </article>
   </section>
 </template>
 
@@ -13,30 +64,30 @@
 import gql from 'graphql-tag'
 import { Codes } from '@/assets/graphql/queries'
 export default {
-    data () {
-        return {
-            columns: [
-                { field: 'type', label: '구분' },
-                { field: 'code', label: '코드' },
-                { field: 'value', label: '값' },
-            ],
-            data: [],
-        }
-    },
-    beforeMount () {
-        let data = []
-        this.$apollo.query({
-            query: gql`${Codes}`,
-        }).then(({ data: { colleges, departments } }) => {
-            colleges.forEach(item => {
-                data.push({ type: '학부', code: item.college_cd, value: item.college_nm })
-            })
-            departments.forEach(item => {
-                data.push({ type: '학과', code: item.dpt_cd, value: item.dpt_nm })
-            })
-            this.data = data
-        })
+  data () {
+    return {
+      columns: [
+        { field: 'type', label: '구분' },
+        { field: 'code', label: '코드' },
+        { field: 'value', label: '값' }
+      ],
+      data: []
     }
+  },
+  beforeMount () {
+    const data = []
+    this.$apollo.query({
+      query: gql`${Codes}`
+    }).then(({ data: { colleges, departments } }) => {
+      colleges.forEach(item => {
+        data.push({ type: '학부', code: item.college_cd, value: item.college_nm })
+      })
+      departments.forEach(item => {
+        data.push({ type: '학과', code: item.dpt_cd, value: item.dpt_nm })
+      })
+      this.data = data
+    })
+  }
 }
 </script>
 
